ac8e8c5e8758082f00dc7b90dacb7b07567148da
Usuario <Usuario@CPD03-M09>
2022-03-23 15:36:21 -0300

    Adicionar arquivos de projeto.

---
+++ AppJogoVelha/AppJogoVelha/MainPage.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppJogoVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+        int jogadas = 0;
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            // Identificando qual botão disparou o evento
+            Button btn = (Button)sender;
+
+            
+
+            // Desabilitando o botão
+            btn.IsEnabled = false;
+
+            // Trocando texto do botão de acordo com quem é a vez.
+            if (vez == "X")
+            {
+                btn.Text = "X";
+                btn.TextColor = Color.Black;
+                btn.FontSize = 35;
+                btn.BackgroundColor = Color.Red;
+               
+                vez = "O";
+                jogadas++;
+            } else
+            {
+                btn.Text = "O";
+                btn.TextColor = Color.Black;
+                btn.FontSize = 35;
+                btn.BackgroundColor = Color.Blue;
+                
+                vez = "X";
+                jogadas++;
+            }
+
+            // verificando se a primeira linha teve ganhador (horizontal
+            if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();            
+
+            } else if (btn10.Text == "O" && btn11.Text == "O" && btn12.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+            if (btn20.Text == "X" && btn21.Text == "X" && btn22.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();
+
+            }
+            else if (btn20.Text == "O" && btn21.Text == "O" && btn22.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+            if (btn30.Text == "X" && btn31.Text == "X" && btn32.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();
+
+            }
+            else if (btn30.Text == "O" && btn31.Text == "O" && btn32.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+            //(
+
+            if (btn10.Text == "X" && btn20.Text == "X" && btn30.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();
+
+            }
+            else if (btn10.Text == "O" && btn20.Text == "O" && btn30.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+            if (btn11.Text == "X" && btn21.Text == "X" && btn31.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();
+
+            }
+            else if (btn11.Text == "O" && btn21.Text == "O" && btn31.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+            if (btn12.Text == "X" && btn22.Text == "X" && btn32.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();
+
+            }
+            else if (btn12.Text == "O" && btn22.Text == "O" && btn32.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+
+            //diagonal
+
+            if (btn10.Text == "X" && btn21.Text == "X" && btn32.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();
+
+            }
+            else if (btn10.Text == "O" && btn21.Text == "O" && btn32.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+            if (btn12.Text == "X" && btn21.Text == "X" && btn30.Text == "X")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou X!!!!", "OK");
+                zerar();
+
+            }
+            else if (btn12.Text == "O" && btn21.Text == "O" && btn30.Text == "O")
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou O!!!!", "OK");
+                zerar();
+            }
+
+            if (jogadas == 9)
+            {
+                await DisplayAlert("Não temos um vencedor!", "O jogo deu velha! EMPATE.", "OK");
+                zerar();
+            }
+
+            if (vez == "X")
+            {
+                label_vez.Text = "É a vez do X!";
+                label_vez.TextColor = Color.Red;
+            }
+            if (vez == "O")
+            {
+                label_vez.Text = "É a vez do O!";
+                label_vez.TextColor = Color.Blue;
+            }
+
+        }
+    
+    
+        void zerar()
+        {
+            btn10.IsEnabled = true;
+            btn10.Text = "";
+            btn11.IsEnabled = true;
+            btn11.Text = "";
+            btn12.IsEnabled = true;
+            btn12.Text = "";
+            btn20.IsEnabled = true;
+            btn20.Text = "";
+            btn21.IsEnabled = true;
+            btn21.Text = "";
+            btn22.IsEnabled = true;
+            btn22.Text = "";
+            btn30.IsEnabled = true;
+            btn30.Text = "";
+            btn31.IsEnabled = true;
+            btn31.Text = "";
+            btn32.IsEnabled = true;
+            btn32.Text = "";
+            vez = "X";
+            jogadas = 0;
+            btn10.BackgroundColor = Color.Gray;
+            btn11.BackgroundColor = Color.Gray;
+            btn12.BackgroundColor = Color.Gray;
+            btn20.BackgroundColor = Color.Gray;
+            btn21.BackgroundColor = Color.Gray;
+            btn22.BackgroundColor = Color.Gray;
+            btn30.BackgroundColor = Color.Gray;
+            btn31.BackgroundColor = Color.Gray;
+            btn32.BackgroundColor = Color.Gray;
+
+
+        }  
+
+        
+    }
+}
